ea808dabe3051beb1cae82cd7f58e538282c11f3
ricardoArana <ricardo.romero@iesdonana.org>
2021-12-01 19:30:16 +0100

    un poco hecho

--- aereopuerto/routes/web.php
+++ aereopuerto/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', [UsuariosController::class, 'loginForm']);
+Route::post('/login', [UsuariosController::class, 'login']);
+Route::post('/logout', [UsuariosController::class, 'logout']);
